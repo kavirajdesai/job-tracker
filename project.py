810c5876ae4223cec14dac306960306dfc9b557c
kavirajdesai <kavirajds22@gmail.com>
2026-05-03 13:14:26 -0400

    Validate Date Function

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+import re
+
 class Job:
     def __init__(self, company, role, date, status, notes):
         self.company= company
@@ -20,7 +22,24 @@ class Job:
     def __str__(self):
         return (f"{self.company} | {self.role} | {self.date} | {self.status} | {self.notes}")
     
-if __name__ == "__main__":
-    job = Job("Google", "Data Analyst", "2025-05-01", "Applied", "LinkedIn")
-    print(job)
-    print(job.to_dict())
+
+# Date Validation:
+def validate_date(date):
+    if (matches:= re.fullmatch(r"\d{4}-(?P<month>1[0-2]|0[1-9])-(?P<days>0[1-9]|[1-2][0-9]|3[0-1])", date)):
+        return True
+    else:
+        return False
+
+
+#TEST FUNCTIONS
+print(validate_date("2025-05-01"))   # True
+print(validate_date("01-05-2025"))   # False
+print(validate_date("abcd-ef-gh"))   # False
+
+# if __name__ == "__main__":
+#     job = Job("Google", "Data Analyst", "2025-05-01", "Applied", "LinkedIn")
+#     print(job)
+#     print(job.to_dict())
+
+
+ 
