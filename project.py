2f97203086693b3fb07b59eed3442d6bf3570415
kavirajdesai <kavirajds22@gmail.com>
2026-05-03 14:21:55 -0400

    Job Class, R/W csv, Validators

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import re
+import csv
 
 class Job:
     def __init__(self, company, role, date, status, notes):
@@ -23,12 +24,33 @@ class Job:
         return (f"{self.company} | {self.role} | {self.date} | {self.status} | {self.notes}")
     
 
-# Date Validation:
+# 1. Date Validation:
 def validate_date(date):
     return bool(re.fullmatch(r"\d{4}-(?P<month>1[0-2]|0[1-9])-(?P<days>0[1-9]|[1-2][0-9]|3[0-1])", date))
 
+# 2. Status Validation:
 def validate_status(status):
     return status.lower() in ("applied", "interview", "offer", "rejected")
+
+# 3. Append Job:
+def save_job(job):
+    with open("application.csv", "a", newline="") as file:
+        writer= csv.DictWriter(file, fieldnames=["company", "role", "date", "status", "notes"])
+        job= job.to_dict()
+        writer.writerow(job)
+
+# 4. Load Jobs:
+def load_jobs():
+    job_list=[]
+    with open("application.csv", "r") as file:
+        reader=csv.DictReader(file)
+        for row in reader:
+            job_list.append(Job(row["company"], row["role"], row["date"], row["status"], row["notes"]))
+    
+    return job_list       
+                
+
+
 
 '''TEST FUNCTIONS'''
 
@@ -44,10 +66,20 @@ def validate_status(status):
 # print(validate_status("Maybe"))      # False
 # print(validate_status("applied"))    # True — case insensitive
 
+
+# 3. Save Job:
+# job = Job("Apple", "Data Analyst", "2025-05-01", "Applied", "Handshake")
+# save_job(job)
+
+# 4. Load Jobs:
+# jobs = load_jobs()
+# for job in jobs:
+#     print(job)
+
+
 # if __name__ == "__main__":
 #     job = Job("Google", "Data Analyst", "2025-05-01", "Applied", "LinkedIn")
 #     print(job)
 #     print(job.to_dict())
-
 
  
