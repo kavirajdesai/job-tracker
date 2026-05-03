1389e7795bd964e65f3a0d0ca090b9973a4f35cc
kavirajdesai <kavirajds22@gmail.com>
2026-05-03 13:27:18 -0400

    Status Validation Function

--- project.py
+++ project.py
@@ -25,16 +25,24 @@ class Job:
 
 # Date Validation:
 def validate_date(date):
-    if (matches:= re.fullmatch(r"\d{4}-(?P<month>1[0-2]|0[1-9])-(?P<days>0[1-9]|[1-2][0-9]|3[0-1])", date)):
-        return True
-    else:
-        return False
+    return bool(re.fullmatch(r"\d{4}-(?P<month>1[0-2]|0[1-9])-(?P<days>0[1-9]|[1-2][0-9]|3[0-1])", date))
+
+def validate_status(status):
+    return status.lower() in ("applied", "interview", "offer", "rejected")
+
+'''TEST FUNCTIONS'''
+
+# 1. Date Validation:
+# print(validate_date("2025-05-01"))   # True
+# print(validate_date("01-05-2025"))   # False
+# print(validate_date("abcd-ef-gh"))   # False
 
 
-#TEST FUNCTIONS
-print(validate_date("2025-05-01"))   # True
-print(validate_date("01-05-2025"))   # False
-print(validate_date("abcd-ef-gh"))   # False
+# 2. Status Validation:
+# print(validate_status("Applied"))    # True
+# print(validate_status("Rejected"))   # True
+# print(validate_status("Maybe"))      # False
+# print(validate_status("applied"))    # True — case insensitive
 
 # if __name__ == "__main__":
 #     job = Job("Google", "Data Analyst", "2025-05-01", "Applied", "LinkedIn")
